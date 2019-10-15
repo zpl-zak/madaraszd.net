0895bf5df6181e3c609d693755a2ddad8de2af90
Dominik Madarász <zaklaus@outlook.com>
2019-10-15 17:41:05 +0200

    Improve Resume design and layout

--- components/Technologies.js
+++ components/Technologies.js
@@ -1,21 +1,20 @@
 export default () => (
     <ul>
-        <li>Git, Mercurial, Subversion</li>
-        <li>Linux admin, SSH, *nix shells (bash, zsh, ..)</li>
+        <li>Git, Mercurial, SVN</li>
+        <li>Linux admin, SSH, *nix shells</li>
         <li>Windows admin, PowerShell</li>
-        <li>C, C++, Go, Python, JavaScript, C#, Java, PHP, SQL</li>
+        <li>C, C++, Go, C#, Java</li>
+        <li>Python, JavaScript, PHP, SQL</li>
         <li>Unity, Unreal Engine 4, Quake</li>
         <li>MySQL, MongoDB, MSSQL</li>
         <li>Less, Sass</li>
         <li>HTML5, Handlebars</li>
         <li>Performance and scalability optimization</li>
         <li>Data management and analysis</li>
-        <li>Composer</li>
         <li>REST, WebSockets</li>
         <li>jQuery, js plugins</li>
         <li>NodeJS, ASP.NET, socket programming</li>
-        <li>CSS3, Bootstrap, Materialize, Markdown, Font Awesome, reStructured</li>
-        <li>Computer Graphics, Physics simulation, Game networking, Game scripting, Gamedev</li>
-        <li>Project planning, stories, sprints, ...</li>
+        <li>CSS3, Bootstrap, Materialize, Markdown</li>
+        <li>CG, PhysSim, Game networking, ...</li>
     </ul>
 )
