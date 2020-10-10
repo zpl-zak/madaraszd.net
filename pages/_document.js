f365e52df721ba9d0adb435fd79c4a6dc45e16fa
Dominik Madarász <zaklaus@outlook.com>
2020-10-10 16:41:36 +0200

    Remove Yandex ru

--- pages/_document.js
+++ pages/_document.js
@@ -26,16 +26,6 @@ export default class PersonalDocument extends Document {
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', '${GA_TRACKING_ID}');
-                    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-                     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-                     ym(56141104, "init", {
-                          clickmap:true,
-                          trackLinks:true,
-                          accurateTrackBounce:true,
-                          webvisor:true
-                    });
                   `
                 }}
               />
