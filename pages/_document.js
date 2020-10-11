78529903c8ac9f386693c965791b31408cfecc8a
Dominik Madarász <zaklaus@outlook.com>
2020-10-11 02:33:02 +0200

    Remove analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,5 @@
 import Document, {Head, Main, NextScript} from 'next/document'
 
-import { GA_TRACKING_ID } from '../lib/gtag'
-
 export default class PersonalDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
@@ -15,20 +13,6 @@ export default class PersonalDocument extends Document {
             <body>
               <Main />
               <NextScript />
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', '${GA_TRACKING_ID}');
-                  `
-                }}
-              />
             </body>
           </html>
         )
