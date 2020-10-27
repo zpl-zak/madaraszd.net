3e9d3414cb14bbf0218f1fde2e8a5731a5bd4c8d
Dominik Madarász <zaklaus@outlook.com>
2020-10-27 14:14:49 +0100

    Update CV

--- components/Technologies.js
+++ components/Technologies.js
@@ -4,15 +4,17 @@ const Technologies = () => (
         <li>Linux admin, SSH, *nix shells</li>
         <li>Windows admin, PowerShell</li>
         <li>C, C++, Go, C#, Java</li>
-        <li>Python, JavaScript, PHP, SQL</li>
+        <li>Python, JavaScript, PHP7, SQL</li>
         <li>Unity, Unreal Engine 4, Quake</li>
-        <li>MySQL, MongoDB, MSSQL</li>
+        <li>MySQL, MongoDB, MSSQL, Redis</li>
         <li>Less, Sass</li>
         <li>OpenGL, Direct3D, Vulkan</li>
         <li>HTML5, Handlebars</li>
         <li>Performance and scalability optimization</li>
         <li>Data management and analysis</li>
         <li>REST, WebSockets</li>
+        <li>Vagrant, Docker, Hyper-V</li>
+        <li>IIS, nginx, Apache</li>
         <li>jQuery, js plugins</li>
         <li>NodeJS, ASP.NET, socket programming</li>
         <li>CSS3, Bootstrap, Materialize, Markdown</li>
