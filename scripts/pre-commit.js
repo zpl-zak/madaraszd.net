7fcc504f9b12b4e2874c2a9f48f967930b653eee
Dominik Madarasz <zaklaus@outlook.com>
2023-01-06 07:49:48 +0100

    fix buffer

--- scripts/pre-commit.js
+++ scripts/pre-commit.js
@@ -1,38 +1,47 @@
-const fs = require('fs')
-const path = require('path')
-const fetch = require('isomorphic-unfetch')
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const fs = require("fs");
+const path = require("path");
+const fetch = require("isomorphic-unfetch");
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
 
-const port = 63423
-const app = next({ dev: true })
-const handle = app.getRequestHandler()
+const port = 63423;
+const app = next({ dev: true });
+const handle = app.getRequestHandler();
 
 const main = async () => {
-    await app.prepare()
+  await app.prepare();
 
-    const server = createServer((req, res) => {
-        const parsedUrl = parse(req.url, true)
-        handle(req, res, parsedUrl)
+  const server = createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+  });
+
+  await Promise.all(
+    ["en", "sk"].map(async (lang) => {
+      console.log(`[pre-commit] generating resume in ${lang} language ...`);
+      let res = await fetch(
+        `http://localhost:${port}/api/generate-resume?lang=${lang}`
+      );
+      let data = new DataView(await res.arrayBuffer());
+      fs.writeFileSync(
+        path.join(__dirname, "../public/", `resume_${lang}.pdf`),
+        data
+      );
     })
+  );
 
-    server.listen(port, err => {if (err) throw err})
+  console.log("[pre-commit] saved pdf files, stopping temp server");
 
-    await Promise.all(['en', 'sk'].map(async lang => {
-        console.log(`[pre-commit] generating resume in ${lang} language ...`)
-        let res = await fetch(`http://localhost:${port}/api/generate-resume?lang=${lang}`)
-        let data = await res.buffer()
-        fs.writeFileSync(path.join(__dirname, '../public/', `resume_${lang}.pdf`), data)
-    }))
+  server.close();
+  process.exit(0);
+};
 
-    console.log('[pre-commit] saved pdf files, stopping temp server')
-
-    server.close()
-    process.exit(0)
-}
-
-main().catch(err => {
-    console.error(err)
-    process.exit(1)
-})
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
